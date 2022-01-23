20cfdaaf23beaf77a22b0c55343db30a803523eb
dajtmullaj <dajt@codeyellow.nl>
2022-01-23 20:52:21 +0100

    debug statefulness app

--- web_app_chemplot.py
+++ web_app_chemplot.py
@@ -329,8 +329,8 @@ else:
                     if st.session_state.new_plot:
                         with st.spinner(f'Plotting your data in about {time} seconds'):  
                             generate_custom_plot()
-                            
-                    if 'plot_html' in st.custom_plot:
+
+                    if 'custom_plot' in st.session_state:
                         st.bokeh_chart(st.session_state.custom_plot, use_container_width=True)
                         html = file_html(st.session_state.custom_plot, CDN)
                         b64 = base64.b64encode(html.encode()).decode('utf-8')
