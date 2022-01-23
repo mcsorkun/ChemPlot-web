9416a66bb38ec594e5956311f5d392062d951a85
dajtmullaj <dajt@codeyellow.nl>
2022-01-23 20:11:35 +0100

    Added download button

--- web_app_chemplot.py
+++ web_app_chemplot.py
@@ -262,8 +262,6 @@ if dataset == 'Sample Dataset':
         components.html(st.session_state.plot_html, width=900, height=680)
         
         b64 = base64.b64encode(st.session_state.plot_html.encode()).decode('utf-8')
-        btn_download = f'<a href="data:file/html;base64,{b64}" download="interactive_plot.html"><input type="button" value="Download Plot"></a>'
-        #st.markdown(btn_download, unsafe_allow_html=True) 
         st.download_button(
             label="Download Plot",
             data=b64,
@@ -339,8 +337,12 @@ else:
                         st.bokeh_chart(st.session_state.custom_plot, use_container_width=True)
                         html = file_html(st.session_state.custom_plot, CDN)
                         b64 = base64.b64encode(html.encode()).decode('utf-8')
-                        btn_download = f'<a href="data:file/html;base64,{b64}" download="interactive_plot.html"><input type="button" value="Download Plot as HTML"></a>'
-                        st.markdown(btn_download, unsafe_allow_html=True)
+                        st.download_button(
+                            label="Download Plot",
+                            data=b64,
+                            file_name='interactive_plot.html',
+                            mime='file/html',
+                        )
 
 add_session_info()
     
