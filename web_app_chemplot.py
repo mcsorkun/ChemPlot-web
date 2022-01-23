a04ce4b9112aaf1b4ee4432923d12aee6583cc55
dajtmullaj <dajt@codeyellow.nl>
2022-01-24 00:52:53 +0100

    add session info

--- web_app_chemplot.py
+++ web_app_chemplot.py
@@ -100,7 +100,8 @@ st.session_state.id = 0
 def add_session_info(plot, name, length, gen_t, sim, dim, p_type):
     st.session_state.id += 1
     now = datetime.now()
-    worksheet.append_row([st.session_state.id, now, plot, name, length, gen_t, sim, dim, p_type])
+    worksheet.append_row([st.session_state.id])
+    #now, plot, name, length, gen_t, sim, dim, p_type])
 
 #########################
 # Session state functions
