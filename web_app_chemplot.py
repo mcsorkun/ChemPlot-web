2dfa7722773c75cf29f0e717ed98972f8fd6a791
dajtmullaj <dajt@codeyellow.nl>
2022-01-24 01:55:40 +0100

    Fixed bug with log error

--- web_app_chemplot.py
+++ web_app_chemplot.py
@@ -112,7 +112,7 @@ def log_error_info(smiles, targets, error):
         values = list(zip(smiles, targets, [error]))
         worksheet.update([['SMILES', 'targets', 'ERROR']] + values)
     else:
-        worksheet.update([['SMILES']] + [smiles])
+        worksheet.update(['SMILES'] + smiles)
        # worksheet.update('B1', 'ERROR')
        # worksheet.update('B2', error)
 
